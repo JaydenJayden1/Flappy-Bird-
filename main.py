ff6b175e792b6b77bd483e663ac12828bdad535a
JaydenJayden1 <jaydenle-coderschoolpasadena@outlook.com>
2025-04-05 13:59:25 -0700

    Make progress

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import pygame
 pygame.init()
 background=pygame.display.set_mode((400,600))
-background.fill("blue")
+clock= pygame.time.Clock()
+player = pygame.image.load('Flappy bird.png').convert_alpha()
+player = pygame.transform.smoothscale(player, (100,100))
+player = player.subsurface(player.get_bounding_rect())
+player_pos = pygame.Vector2(20,240)
+why_speed = 0
+pipe = pygame.image.load('Pipes.png')
 
 game_running = True
-while game_running:
+while game_running: 
+    jump=False
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_running = False
+        if event.type==pygame.QUIT:
+            game_running=False
             pygame.quit()
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_SPACE or event.key==pygame.K_UP:
+                jump=True
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            if event.button==1:
+                jump=True
+    
+    background.fill("sky blue")
+    why_speed+=.26
+    if jump:
+        why_speed=-5
+    player_pos.y+=why_speed
+    background.blit(player,player_pos)
     pygame.display.flip()
+    clock.tick(60)
