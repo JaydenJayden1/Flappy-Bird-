4bf95402e1f36fc3fee6accd35d0d7a9209395f8
JaydenJayden1 <jaydenle-coderschoolpasadena@outlook.com>
2025-04-26 13:10:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ player = player.subsurface(player.get_bounding_rect())
 player_pos = pygame.Vector2(20,240)
 why_speed = 0
 pipe = pygame.image.load('Pipes.png')
+
 game_running = True
 while game_running: 
     jump=False
@@ -21,6 +22,7 @@ while game_running:
         if event.type==pygame.MOUSEBUTTONDOWN:
             if event.button==1:
                 jump=True
+                
     background.fill("sky blue")
     why_speed+=.26
     if jump:
