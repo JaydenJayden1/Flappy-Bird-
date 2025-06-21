d9c35dc5b935b4e245da610b71efc2b65f1dbe62
JaydenJayden1 <jaydenle-coderschoolpasadena@outlook.com>
2025-06-21 14:03:47 -0700

    https://github.com/JaydenJayden1/Flappy-Bird-.git

--- Test.py
+++ Test.py
@@ -1,4 +1,5 @@
 # Example file showing a basic pygame "game loop"
+#
 import pygame
 
 # pygame setup
@@ -6,6 +7,12 @@ pygame.init()
 screen = pygame.display.set_mode((1920, 1080))
 clock = pygame.time.Clock()
 running = True
+background_image = pygame.image.load('Space_S.webp')
+background_image = pygame.transform.scale(background_image,[1920,1080])
+background_rect = background_image.get_rect()
+
+
+
 
 player = pygame.Vector2(1920, 1080)/2
 angle = 180
@@ -22,8 +29,8 @@ while running:
     speed.x += keys[pygame.K_UP] - keys[pygame.K_DOWN]
     speed *= .75
     player += speed.rotate(angle)
+    screen.blit(background_image, background_rect)
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("grey15")
     pygame.draw.circle(screen, "white", player, 20)
     pygame.draw.line(screen , "red" , player , player + pygame.Vector2(21, 0).rotate(angle))
 
